af36333d294c0a0a9c477a93c60d60e3e0b0b735
Cowwen Lin <cowwen@gmail.com>
2012-06-22 10:23:25 +0800

    modify the Hello.java file to look compare.

--- src/com/lin/hello/Hello.java
+++ src/com/lin/hello/Hello.java
@@ -1,7 +1,11 @@
 package com.lin.hello;
 
 public class Hello {
+	/**
+	 * @param 用户输入参数.
+	 * */
 	public static void main(String[] args) {
 		System.out.println("Hello git,nice to me you!");
+		System.out.println("use egit function.");
 	}
 }
